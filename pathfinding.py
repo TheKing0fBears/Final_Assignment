99856bb25e3e1fcc8b4a70ca08bac03e8f4366b3
TheKing0fBears <90875696+TheKing0fBears@users.noreply.github.com>
2021-12-16 17:11:22 -0800

    Update pathfinding.py

--- pathfinding.py
+++ pathfinding.py
@@ -1,1 +1,20 @@
 import pygame
+import math
+from queue import PriorityQueue
+
+SIZE = 1000
+window = pygame.display.set_mode((SIZE, SIZE))
+pygame.display.set_caption("Pathfinding assignment")
+
+BLACK = (0,0,0)
+
+class Node:
+  def __init__(self,row,col,width,total_rows)
+    self.row = row#simplification of variables for later use
+    self.col = col
+    self.x=row*width
+    self.y=col.width
+    self.color=BLACK
+    self.neighbors = []
+    self.width = width
+    
